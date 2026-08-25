02c5b1142e0937348a5cc89795723ddfce53a28f
dlgjr <985039081@qq.com>
2026-08-26 02:20:01 +0800

    handle unused multimodal parameters in GSPO DDP

--- scripts/dlc/start_gspo.sh
+++ scripts/dlc/start_gspo.sh
@@ -40,6 +40,7 @@ mkdir -p \
   "$CUDA_CACHE_PATH" \
   "$PYTHONPYCACHEPREFIX" \
   "$XDG_CACHE_HOME" \
+  "$XDG_CACHE_HOME/torch/kernels" \
   "$MPLCONFIGDIR" \
   "$TORCHINDUCTOR_CACHE_DIR" \
   "$TRITON_CACHE_DIR"
@@ -171,6 +172,7 @@ if [[ "$NODE_RANK" == "0" ]]; then
   echo "judge_serve_name=$GSPO_JUDGE_SERVE_NAME judge_max_tokens=$GSPO_JUDGE_MAX_TOKENS judge_tp=$GSPO_JUDGE_TENSOR_PARALLEL_SIZE judge_thinking=false"
   echo "epochs=$GSPO_NUM_TRAIN_EPOCHS generations=$GSPO_NUM_GENERATIONS iterations=$GSPO_NUM_ITERATIONS steps_per_generation=$GSPO_STEPS_PER_GENERATION generation_batch=$GSPO_GENERATION_BATCH_SIZE"
   echo "max_length=$GSPO_MAX_LENGTH max_completion_length=$GSPO_MAX_COMPLETION_LENGTH save_steps=$GSPO_SAVE_STEPS logging_steps=$GSPO_LOGGING_STEPS log_entropy=$GSPO_LOG_ENTROPY"
+  echo "gradient_checkpointing=$GSPO_GRADIENT_CHECKPOINTING gradient_checkpointing_kwargs=$GSPO_GRADIENT_CHECKPOINTING_KWARGS ddp_find_unused_parameters=$GSPO_DDP_FIND_UNUSED_PARAMETERS"
   echo "kl_beta=$GSPO_BETA entropy_coef=$GSPO_ENTROPY_COEF"
   echo "top_reward_steps=$GSPO_TOP_REWARD_STEPS top_reward_k=$GSPO_TOP_REWARD_K"
   echo "vllm_mode=$GSPO_VLLM_MODE vllm_max_model_len=$GSPO_VLLM_MAX_MODEL_LEN vllm_max_num_seqs=$GSPO_VLLM_MAX_NUM_SEQS"
@@ -196,6 +198,8 @@ ARGS=(
   --gradient_accumulation_steps "$GSPO_GRAD_ACC"
   --gradient_checkpointing "$GSPO_GRADIENT_CHECKPOINTING"
   --vit_gradient_checkpointing "$GSPO_VIT_GRADIENT_CHECKPOINTING"
+  --gradient_checkpointing_kwargs "$GSPO_GRADIENT_CHECKPOINTING_KWARGS"
+  --ddp_find_unused_parameters "$GSPO_DDP_FIND_UNUSED_PARAMETERS"
   --num_train_epochs "$GSPO_NUM_TRAIN_EPOCHS"
   --num_generations "$GSPO_NUM_GENERATIONS"
   --num_iterations "$GSPO_NUM_ITERATIONS"
